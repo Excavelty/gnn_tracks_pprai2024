4e5c02b22730fea541c163c8561dd996fdb8281f
Excavelty <gambitek2@gmail.com>
2024-02-14 02:27:58 +0100

    First attempts

---
+++ prepare_data.py
@@ -0,0 +1,139 @@
+import torch
+import pandas as pd
+import json
+import os
+from torch_geometric import seed_everything
+from torch_geometric.data import Data
+import torch_geometric.transforms as T
+
+# initial seed for reproducibility
+seed_everything(1000)
+
+# Experiment concept:
+# we take already reconstructed tracks and hits to create graph for training network
+# to predict existance of the edge
+# therefore we create "real" edges between elements belonging to the same track
+# and RandomLinkSplit will label them as 1s and create same number of "false" edges
+# and label them as 0s. The task is to check how well different architectures
+# will learn to distinguish between those labels 
+
+# function preparing input graph by taking one event (singe hits file and tracks_ambi file)
+def prepare_graph_from_file(hits_file_path, tracks_file_path):
+    # Read all hits from the file
+    df = pd.read_csv(hits_file_path, engine='python', delimiter=',')
+    
+    # Prepare features of graph nodes, hit will be treated as a node
+    x_s = df['tx'].values.tolist()
+    y_s = df['ty'].values.tolist()
+    z_s = df['tz'].values.tolist()
+
+    # squash into features tensor and transpose
+    node_features = torch.tensor([x_s, y_s, z_s], dtype=torch.float).t()
+
+    # Read all tracks and use them to construct edge_index
+    df = pd.read_csv(tracks_file_path, engine='python', delimiter=',')
+    filter = df['good/duplicate/fake'] == 'good'
+    
+    tracks_strings = df.where(filter)['Hits_ID'].tolist()
+    tracks_lists = [json.loads(track_string.replace(',]', ']')) for track_string in tracks_strings]
+
+    edge_list = list()
+
+    for track_list in tracks_lists:
+        # create edges between every two consecutive elements of the particle track
+        edge_list.extend([[track_list[i], track_list[i + 1]] for i in range(len(track_list) - 1)])
+
+    edge_index = torch.tensor(edge_list, dtype=torch.long).t()
+
+    print(edge_index.size())
+
+    data = Data(node_features, edge_index)
+    print(data)
+    transform = T.Compose([T.NormalizeFeatures(), T.RandomLinkSplit(add_negative_train_samples=True, disjoint_train_ratio=0.3)])
+    train_data, val_data, test_data = transform(data)
+
+    print(train_data)
+    print(test_data)
+    print(val_data)
+
+    return train_data, test_data, val_data
+
+# function preparing data from single pair of hits/tracks_ambi files
+def prepare_data_from_file(hits_file_path, tracks_file_path):
+    # Read all hits from the file
+    df = pd.read_csv(hits_file_path, engine='python', delimiter=',')
+    
+    # Prepare features of graph nodes, hit will be treated as a node
+    x_s = df['tx'].values.tolist()
+    y_s = df['ty'].values.tolist()
+    z_s = df['tz'].values.tolist()
+
+    # Read all tracks and use them to construct edge_index
+    df = pd.read_csv(tracks_file_path, engine='python', delimiter=',')
+    filter = df['good/duplicate/fake'] == 'good'
+    
+    tracks_strings = df.where(filter)['Hits_ID'].tolist()
+    tracks_lists = [json.loads(track_string.replace(',]', ']')) for track_string in tracks_strings]
+
+    edge_list = list()
+
+    for track_list in tracks_lists:
+        edge_list.extend([[track_list[i], track_list[i + 1]] for i in range(len(track_list) - 1)])
+
+    return x_s, y_s, z_s, edge_list
+
+# CONTROVERSIAL!!!
+# Function preparing graph as connection of graphs from different events
+def prepare_graph_from_multiple_files(path, number_of_files):
+    file_names = os.listdir(path)
+
+    x_all = list()
+    y_all = list()
+    z_all = list()
+    edge_all = list()
+
+    hits_file = tracks_file = None
+
+    iter = 0
+
+    for file_name in file_names:
+        if 'hits' in file_name:
+            hits_file = path + '/' + file_name
+
+        if 'tracks_ambi' in file_name:
+            tracks_file = path + '/' + file_name
+
+        if hits_file is not None and tracks_file is not None:
+            x_s, y_s, z_s, edge_list = prepare_data_from_file(hits_file, tracks_file)
+
+            x_all.extend(x_s)
+            y_all.extend(y_s)
+            z_all.extend(z_s)
+            edge_all.extend(edge_list)
+
+            iter += 1
+
+            print(f'{iter}. files prepared')
+
+            hits_file = None
+            tracks_file = None
+
+        if iter >= number_of_files:
+            break
+    
+    
+    node_features = torch.tensor([x_all, y_all, z_all], dtype=torch.float).t()
+    edge_index = torch.tensor(edge_all, dtype=torch.long).t()
+
+    print(edge_index.size())
+
+    data = Data(node_features, edge_index)
+    print(data)
+    transform = T.Compose([T.NormalizeFeatures(), T.RandomLinkSplit(add_negative_train_samples=True, disjoint_train_ratio=0.3)])
+    train_data, val_data, test_data = transform(data)
+
+    print(train_data)
+    print(test_data)
+    print(val_data)
+
+    return train_data, test_data, val_data
---
+++ run_model.py
@@ -0,0 +1,153 @@
+# Template of the model based on https://medium.com/@pytorch_geometric/link-prediction-on-heterogeneous-graphs-with-pyg-6d5c29677c70
+
+import torch
+from torch_geometric.data import Data
+import torch.nn.functional as F
+import torch_geometric.transforms as T
+from torch_geometric.nn import SAGEConv, GCNConv, GATConv, GatedGraphConv
+from prepare_data import prepare_data_from_file, prepare_graph_from_file, prepare_graph_from_multiple_files
+
+def train_model(model, optimizer, data_train):
+    num_of_epochs = 10
+    
+    model.train()
+    
+    # total_loss = total_examples = 0
+    for epoch in range(num_of_epochs):
+        print(epoch + 1)
+        optimizer.zero_grad()
+        out = model(data_train)
+
+        loss = F.binary_cross_entropy_with_logits(out, data_train.edge_label)
+        loss.backward()
+
+        optimizer.step()        
+        # total_loss += float(loss) * out.numel()
+        # total_examples += out.numel()
+    
+        # print(f"Epoch: {epoch:03d}, Loss: {total_loss / total_examples:.4f}")
+
+        # calculate metrics after final epoch
+        if epoch == num_of_epochs - 1:
+            tp = 0
+            tn = 0
+            fp = 0
+            fn = 0
+
+            for i, el in enumerate(out):
+                # assume that 0.4 is enough to treat as real connection
+                if el < 0.4:
+                    el = 0
+                else:
+                    el = 1
+
+                if data_train.edge_label[i] == 1:
+                    if el == 1:
+                        tp += 1
+                    else:
+                        fp += 1
+
+                if data_train.edge_label[i] == 0:
+                    if el == 0:
+                        tn += 1
+                    else:
+                        fn += 1
+
+            print("####")
+            print(f'Sensitivity = TP / (TP + FN) = {tp / (tp + fn)}')
+            print(f'Specificity = TN / (TN + FP) = {tn / (tn + fp)}')
+            print(f'Precision = TP / (TP + FN) = {tp / (tp + fn)}')
+            print(f'Negative predictive value = TN / (TN + FN) = {tn / (tn + fn)}')
+            print(f'Accuracy = TP + TN / (TP + TN + FP + FN) = {(tp + tn) / (tp + tn + fp + fn)}')
+            print("####")
+
+# TBA
+# def test_model()
+
+class SAGEConvModel(torch.nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.conv1 = SAGEConv(3, channels)
+        self.conv2 = SAGEConv(channels, channels)
+        self.conv3 = SAGEConv(channels, channels)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = self.conv2(x, edge_index)
+        x = F.relu(x)
+        x = self.conv3(x, edge_index)
+
+        return x
+
+class GCNConvModel(torch.nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.conv1 = GCNConv(3, channels)
+        self.conv2 = GCNConv(channels, channels)
+        self.conv3 = GCNConv(channels, channels) 
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = self.conv2(x, edge_index)
+        x = F.relu(x)
+        x = self.conv3(x, edge_index)
+
+        return x
+
+class GATConvModel(torch.nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.conv1 = GATConv(3, channels)
+        self.conv2 = GATConv(channels, channels)
+        self.conv3 = GATConv(channels, channels)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = self.conv2(x, edge_index)
+        x = F.relu(x)
+        x = self.conv3(x, edge_index)
+
+        return x 
+
+class Classifier(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x, edge_label_index):
+        from_edges = x[edge_label_index[0]] 
+        to_edges = x[edge_label_index[1]]
+        
+        return (from_edges * to_edges).sum(dim=-1)
+
+class Model(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super().__init__()
+
+        self.GNN = GatedGraphConv(hidden_channels, 10)
+        self.Classifier = Classifier()
+
+    def forward(self, data):
+        x = self.GNN(data.x, data.edge_index)
+        result = self.Classifier(x, data.edge_label_index)
+
+        return result
+
+if __name__ == '__main__':
+    # training based on single event
+    # data_train, data_test, data_val = prepare_graph_from_file(hits_file_path='..\odd_output\odd_output\event000000000-hits.csv',
+    #                                                           tracks_file_path='..\odd_output\odd_output\event000000000-tracks_ambi.csv')
+
+    # connecting events
+    data_train, data_test, data_val = prepare_graph_from_multiple_files(path='../odd_output/odd_output', number_of_files=10)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    model = Model(hidden_channels=256).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    train_model(model, optimizer, data_train)
+
+    # print(F.binary_cross_entropy_with_logits(pred, data_test.edge_label))
